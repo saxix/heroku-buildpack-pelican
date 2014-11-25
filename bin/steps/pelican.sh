d6f5778d1d5ddbc69fc761c0c8565a22d3315bc8
sax <s.apostolico@gmail.com>
2014-11-25 15:23:48 +0100

    updates pelican step

--- bin/steps/pelican.sh
+++ bin/steps/pelican.sh
@@ -1,5 +1,5 @@
 puts-step "Installing Pelican"
-pip install --use-mirrors pelican | indent
+pip install -U --use-mirrors pelican | indent
 
 puts-step "Installing Markdown"
 pip install --use-mirrors Markdown | indent
