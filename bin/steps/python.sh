3cde741adf36774336d4b7e357a9ee9bd07c9cbc
sax <s.apostolico@gmail.com>
2014-11-25 15:08:21 +0100

    restore pelican build

--- bin/steps/python.sh
+++ bin/steps/python.sh
@@ -1,5 +1,4 @@
-#if [[ ! -d "$CACHE_DIR/$PYTHON_VERSION" ]]; then
-if [[ ! -e "$CACHE_DIR/$PYTHON_VERSION" ]]; then
+if [[ ! -d "$CACHE_DIR/$PYTHON_VERSION" ]]; then
   cd $CACHE_DIR
   puts-step "Installing Python ($PYTHON_VERSION)"
   curl http://envy-versions.s3.amazonaws.com/$PYTHON_VERSION.tar.bz2 -s | tar jx &> /dev/null
